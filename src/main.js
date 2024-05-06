c169995c3c22b2bc227c5be844a50a905926b135
Anatol <chernov.py.dev@gmail.com>
2024-05-06 21:06:10 +0300

    add PrimeVue

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-green/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
