cde76842542a886dbdbddc1ce808e246cc115b44
Anatol <chernov.py.dev@gmail.com>
2024-05-10 22:39:18 +0300

    add pinia in main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/saga-green/theme.css'
+// import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import './assets/main.css'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
 app.use(PrimeVue)
+app.use(pinia)
 
 app.mount('#app')
